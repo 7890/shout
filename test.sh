e92cf798df55f46f9b155f4c08e068bd40986574
Thomas Brand <tom@trellis.ch>
2014-08-23 20:52:11 +0200

    init. test shout with several input strings

---
+++ test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#opts="1 0 1"
+opts=""
+
+#read_wait="read a"
+read_wait=""
+
+function test
+{
+	shout "$1" $opts
+	echo "$1" | shout - $opts
+	$read_wait
+}
+
+test "123"
+test "\[123"
+test "\{123"
+test "\(123"
+test "\<123"
+test "\[123\{456\(789"
+test "\<\[123\{456\(789"
+test "\[123\].\{456\}.\(789\)."
+test "\<1\>2\<3\>"
+
+test "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+test "\(aaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+test "\(aa\[aaaaaaaaaaaaaaaaaaaaaaaaaaa"
+test "\(aa\[aa\<aaaaaaaaaaaaaaaaaaaaaaa\>aa"
+
+string="\\\\/0123456789+-=_.,:;!?|%&@#^~[](){}$<>*'\` abcdefghijklmnopqrstuvwxyz"
+test "$string"
+
+printf "a\nb\nc\n" | shout - $opts
+printf "a\nbbbbbbbbbbbbbbbbbbbbbbbbbbb\nc\n" | shout - $opts
